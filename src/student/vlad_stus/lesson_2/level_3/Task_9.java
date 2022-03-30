cae803070916941883d9e056249364bcf26f6074
Prolog952 <prolog952@gmail.com>
2022-03-30 23:08:02 +0300

    vlad-stus-lesson-2-level-2-task-9

--- src/student/vlad_stus/lesson_2/level_3/Task_9.java
+++ src/student/vlad_stus/lesson_2/level_3/Task_9.java
@@ -1,4 +1,21 @@
 package student.vlad_stus.lesson_2.level_3;
 
+import java.util.Scanner;
+
 public class Task_9 {
+
+    public static void main(String[] args) {
+        System.out.println("Введите целое число");
+
+        Scanner scanner = new Scanner (System.in);
+
+        int userNumer = scanner.nextInt();
+
+        int userSum1 = userNumer * 2;
+        int userSum2 = userNumer * 3;
+
+        System.out.println( userNumer + " x " + 1 + " = " + userNumer  );
+        System.out.println( userNumer + " x " + 2 + " = " + userSum1  );
+        System.out.println( userNumer + " x " + 3 + " = " + userSum2  );
+    }
 }
