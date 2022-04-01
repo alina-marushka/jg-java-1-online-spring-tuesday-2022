30b6afe0c548953ceb2c935d939302aa962baf5f
jelen <Leeeeen78>
2022-04-01 19:28:23 +0300

    Lesson 2 Level 7 Task31

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task31.java
@@ -0,0 +1,40 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task31 {
+    public static void main(String[] args) {
+       // java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+       // System.out.println("ведите 1 число : " );
+       // int num1 = scanner.nextInt();
+
+      // System.out.println("ведите 2 число : ");
+       // int num2 = scanner.nextInt();
+
+       // System.out.println("ведите 3 число : ");
+       // int num3 = scanner.nextInt();
+
+       // System.out.println("Результат : ");
+
+       // double res = (num1 + num2 + num3)/3;
+
+       // System.out.println(res);
+
+        /* java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        System.out.println("ведите 1 число : " );
+        int num1 = scanner.nextInt();
+
+        System.out.println("ведите 2 число : ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("ведите 3 число : ");
+        int num3 = scanner.nextInt();
+
+        System.out.println("Результат : ");
+
+        double res = (num1 + num2 + num3)/3;
+
+        System.out.println(res);   */
+
+    }
+}
