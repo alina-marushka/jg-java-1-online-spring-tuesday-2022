ce0340bfae6140b71bc239159608f27dbc997743
jelen <Leeeeen78>
2022-04-01 19:19:14 +0300

    Lesson 2 Level 7 Task28

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task28.java
@@ -0,0 +1,42 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task28 {
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
+
+    /*справьте ошибки компиляции в данной программе.
+
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int numberOne;
+        int numberTwo;
+
+        System.out.println("Number 1 = " + numberOne);
+        System.out.println("Number 2 = " + numberTwo);
+
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        int sum = numberOne + numberTwo;
+
+        System.out.println("Sum = " + sum);
+    }
+
+}
+
+Подсказка: в Java использовать переменные нельзя до тех пор пока
+        им не будет присвоено начальное значение. */
