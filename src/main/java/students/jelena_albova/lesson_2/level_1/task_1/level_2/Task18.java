ba04c1a87fc6a6d6e0e6cf9c2d08ea69a0c0813a
jelen <Leeeeen78>
2022-04-01 16:00:40 +0300

    Lesson 2 Level 5 Task18

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task18.java
@@ -0,0 +1,22 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task18 {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+
+    }
+
+
+}
+
+
+    /*аставьте эту программу работать!
+
+class SizeMatters {
+    public static void main(String[] args) {
+        system.out.println("Sorry, I have an errors.");
+        system.out.println("Please fix me!")
+    }
+}
+*/
