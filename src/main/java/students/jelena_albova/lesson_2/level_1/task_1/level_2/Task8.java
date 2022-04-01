931854968f4f3c109af06eaa0f44e242c11a0f39
jelen <Leeeeen78>
2022-04-01 13:26:03 +0300

    Lesson 2 Level 1 Task8

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task8.java
@@ -0,0 +1,23 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task8 {
+    public static void main(String[] args) {
+
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+
+        System.out.println("Введите имя пользователя: ");
+        String username = scan.nextLine();
+        System.out.println("Hello, " + username);
+
+
+
+    }
+
+
+}
+
+
+
+   /* Написать консольную программу, которая запрашивает
+        имя пользователя и выводит на консоль текст:
+        Hello имя пользователя!*/
