6f4e81fa863d4d22eae81c520865c57a78d316f0
jelen <Leeeeen78>
2022-04-01 13:09:40 +0300

    Lesson 2 Level 1 Task4

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task4Phone.java
@@ -0,0 +1,13 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task4Phone {
+
+
+}
+
+    /*Дан код, нужно исправить ошибку компиляции.
+        Для сдачи работы создайте исправленный класс.
+
+class Phone
+
+}   */
