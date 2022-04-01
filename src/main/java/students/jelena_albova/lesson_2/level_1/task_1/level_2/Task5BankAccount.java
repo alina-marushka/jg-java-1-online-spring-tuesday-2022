f43792e1d0b238d79a6885c502d5ed72a982f6bb
jelen <Leeeeen78>
2022-04-01 13:13:18 +0300

    Lesson 2 Level 1 Task5

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task5BankAccount.java
@@ -0,0 +1,20 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task5BankAccount {
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+
+
+    /*Дан код, нужно исправить ошибку компиляции.
+        Для сдачи работы создайте исправленный класс.
+
+class BankAccount {
+
+    public static void main(String[] args) {
+
+    }*/
