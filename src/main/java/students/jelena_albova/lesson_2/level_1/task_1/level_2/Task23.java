599f36bd51c515de7b766f55068b3cc88fd3c77c
jelen <Leeeeen78>
2022-04-01 16:07:01 +0300

    Lesson 2 Level 6 Task23

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task23.java
@@ -0,0 +1,22 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task23 {
+    public static void main(String[] args) {
+
+        System.out.println("Code outside the method will not compile!");
+
+    }
+
+}
+
+
+    /*Заставьте эту программу работать!
+
+class AttentionPlease {
+
+    System.out.println("Code outside the method will not compile!");
+
+    public static void main(String[] args) {
+
+    }
+}*/
