8d1733e9a4b861a3407f2286c017c135058f46b9
jelen <Leeeeen78>
2022-04-01 16:09:45 +0300

    Lesson 2 Level 6 Task24

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task24.java
@@ -0,0 +1,22 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task24 {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+
+
+}
+
+
+   /* Заставьте эту программу работать!
+
+class NoSleepNoFocus {
+    public static void main(String[] args) {
+        System.out.println(I wanna coffee);
+        System.out.println("So bad);
+    }
+}
+
+PS: Текст (строка) в Java пишется в двойных кавычках. */
