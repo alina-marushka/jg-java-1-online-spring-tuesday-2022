2bc6d69063889025f2b2c25f94cd66a2ccab3b21
jelen <Leeeeen78>
2022-04-01 13:18:56 +0300

    Lesson 2 Level 1 Task7

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task7LoginService.java
@@ -0,0 +1,20 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task7LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+
+    }
+
+
+}
+
+
+    /*Дан код, нужно сделать этот код запускаемым.
+        Для сдачи работы создайте исправленный класс.
+
+class LoginService {
+
+    public void main(String[] args) {
+        System.out.println("You can login!");
+    } */
