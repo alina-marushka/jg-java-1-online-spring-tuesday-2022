de05617594569af8403bdc141eafead1d6b001fd
jelen <Leeeeen78>
2022-04-01 15:45:42 +0300

    Lesson 2 Level 5 Task15

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task15.java
@@ -0,0 +1,24 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task15 {
+
+    public static void main(String[] args) {
+
+        System.out.print("Hello, ");
+        System.out.print("World!");
+
+
+
+
+    }
+}
+   /* Команда System.out.println("text"); выводит указанный "text"
+        на консоль, и переводит курсор на новую строку.
+
+        Для того, что бы выводить текст на консоль без перевода курсора на
+        новую строку можно воспользоваться командой
+        System.out.print("text");
+        эта команда распечатает текст на консоль и оставит курсор в той же строке.
+
+        Напишите программу, которая демонстрирует использование команды
+        System.out.print("text"); */
