20882af6dd70f1f07cea3c0ff34918a9e91884ff
jelen <Leeeeen78>
2022-04-01 13:16:45 +0300

    Lesson 2 Level 1 Task6

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task6Turtle.java
@@ -0,0 +1,21 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+public class Task6Turtle {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+
+   /* Дан код, нужно исправить ошибку компиляции.
+        Для сдачи работы создайте исправленный класс.
+
+class Turtle {
+
+    public static void main(String[] args)
+
+}
+
+}*/
