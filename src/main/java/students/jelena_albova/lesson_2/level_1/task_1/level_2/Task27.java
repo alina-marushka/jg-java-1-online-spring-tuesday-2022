ed904c4f301975b3b99543948be6db3440ce9232
jelen <Leeeeen78>
2022-04-01 16:15:14 +0300

    Lesson 2 Level 7 Task27

---
+++ src/main/java/students/jelena_albova/lesson_2/level_1/task_1/level_2/Task27.java
@@ -0,0 +1,8 @@
+package students.jelena_albova.lesson_2.level_1.task_1.level_2;
+
+        Book
+        BankAccount
+        SystemUnit
+        LoginService
+        BankTransAction
+        Task1
