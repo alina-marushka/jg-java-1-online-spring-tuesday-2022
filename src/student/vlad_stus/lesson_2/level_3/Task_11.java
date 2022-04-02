8de11e3a15c0ce20e9a926963085aafa7782097c
Prolog952 <prolog952@gmail.com>
2022-04-02 16:23:32 +0300

    vlad-stus-lesson-2-level-2-task-11

--- src/student/vlad_stus/lesson_2/level_3/Task_11.java
+++ src/student/vlad_stus/lesson_2/level_3/Task_11.java
@@ -1,9 +1,19 @@
 package student.vlad_stus.lesson_2.level_3;
 
-import.java.util
+import java.util.Scanner;
 
 public class Task_11 {
     public static void main(String[] args) {
-        System.out.println();
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Введите три целых числа");
+        int numberOne = scanner.nextInt();
+        int numberTwo = scanner.nextInt();
+        int numberThree = scanner.nextInt();
+
+        double avarage = (numberOne + numberTwo + numberThree) / 3 ;
+        System.out.println(avarage);
+
+
     }
 }
