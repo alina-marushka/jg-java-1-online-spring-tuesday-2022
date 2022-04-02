33e6de7675304aa8f3850c77a54f1324539b6c82
Prolog952 <prolog952@gmail.com>
2022-04-02 16:38:37 +0300

    vlad-stus-lesson-2-level-4-task-15

---
+++ src/student/vlad_stus/lesson_2/level_4/Task_15.java
@@ -0,0 +1,9 @@
+package student.vlad_stus.lesson_2.level_4;
+
+public class Task_15 {
+    public static void main(String[] args) {
+        System.out.println("Text ");
+
+        System.out.print("Text ");;
+    }
+}
