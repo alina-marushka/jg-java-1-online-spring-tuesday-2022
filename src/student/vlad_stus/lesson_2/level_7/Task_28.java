454f74c74e296c193324ff822789878e86427990
Prolog952 <prolog952@gmail.com>
2022-04-03 18:24:54 +0300

    vlad-stus-lesson-2-level-7-task-28

---
+++ src/student/vlad_stus/lesson_2/level_7/Task_28.java
@@ -0,0 +1,18 @@
+package student.vlad_stus.lesson_2.level_7;
+
+public class Task_28 {
+
+    public static void main(String[] args) {
+
+        int numberOne = 10;
+        int numberTwo = 20;
+
+
+        System.out.println("Number 1 = " + numberOne);
+		System.out.println("Number 2 = " + numberTwo);
+
+    int sum = numberOne + numberTwo;
+
+		System.out.println("Sum = " + sum);
+}
+}
