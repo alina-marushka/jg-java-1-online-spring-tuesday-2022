799d3237527ba7d930e9d52daf570e221b0277aa
roman <romanbogomol@gmail.com>
2022-04-06 00:06:21 +0300

    Lesson 3, Level 2, Task 7-9.

---
+++ src/main/java/students/romans_bogomols/lesson_3/level_2/task_7/MyName.java
@@ -0,0 +1,10 @@
+package students.romans_bogomols.lesson_3.level_2.task_7;
+
+class MyName {
+    public static void main(String[] args) {
+
+        String myName = "Roman";
+        System.out.println(myName);
+
+    }
+}
---
+++ src/main/java/students/romans_bogomols/lesson_3/level_2/task_8/HiMyName.java
@@ -0,0 +1,11 @@
+package students.romans_bogomols.lesson_3.level_2.task_8;
+
+public class HiMyName {
+
+    public static void main(String[] args) {
+      String myName = "Roman";
+      String greeting = "Hi " + myName + "!";
+
+        System.out.println(greeting);
+    }
+}
