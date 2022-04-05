01949c7d6f709312e95fa968e893f536e0cc8a55
georgy <georgy>
2022-04-05 18:59:18 +0300

    Lesson 2, Level 3, Task 11

---
+++ src/main/java/students/georgy_bakulin_lesson_2/level_3/task_11/Task11.java
@@ -0,0 +1,20 @@
+package students.georgy_bakulin_lesson_2.level_3.task_11;
+
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter 3 numbers to calculate average");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        double num3 = scanner.nextDouble();
+
+
+
+        System.out.println("Your numbers average is = " +(num1+num2+num3)/3 );
+    }
+}
