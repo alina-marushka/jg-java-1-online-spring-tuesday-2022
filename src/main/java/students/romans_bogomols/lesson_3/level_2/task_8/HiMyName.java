06820e1ea3e0e4ddbab9778378f5ac088f0cbe0d
roman <romanbogomol@gmail.com>
2022-04-09 00:44:01 +0300

    Lesson 3, Level 2, Task 8, update.

--- src/main/java/students/romans_bogomols/lesson_3/level_2/task_8/HiMyName.java
+++ src/main/java/students/romans_bogomols/lesson_3/level_2/task_8/HiMyName.java
@@ -1,10 +1,10 @@
 package students.romans_bogomols.lesson_3.level_2.task_8;
 
-public class HiMyName {
+class HiMyName {
 
     public static void main(String[] args) {
-      String myName = "Roman";
-      String greeting = "Hi " + myName + "!";
+        String myName = "Roman";
+        String greeting = "Hi " + myName + "!";
 
         System.out.println(greeting);
     }
