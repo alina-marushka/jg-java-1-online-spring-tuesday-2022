ecbedab163fe08022f48192a22e31c6be95812b7
roman <romanbogomol@gmail.com>
2022-04-09 01:11:53 +0300

    Lesson 3, Level 5, Task 17.

---
+++ src/main/java/students/romans_bogomols/lesson_3/level_5/task_17/DogDemo.java
@@ -0,0 +1,10 @@
+package students.romans_bogomols.lesson_3.level_5.task_17;
+
+class DogDemo {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("Smoky");
+        dog.voice();
+    }
+
+}
