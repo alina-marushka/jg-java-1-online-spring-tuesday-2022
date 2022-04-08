e8e8d270608aa77887667178418bdbbb7cd7ccd8
deniss mirosnikovs <dmlv938@gmail.com>
2022-04-08 11:45:04 +0300

    Lesson 3, levels 1-5

---
+++ src/main/java/students/deniss_mirosnikovs/lesson_3/level_1/Robot.java
@@ -0,0 +1,16 @@
+package students.deniss_mirosnikovs.lesson_3.level_1;
+
+class Robot {
+
+    public void sayHello() {
+        System.out.print("Hello! ");
+    }
+
+    public void sayYourName() {
+        System.out.println("My name is ROBO");
+    }
+
+    public Robot() {
+
+    }
+}
---
+++ src/main/java/students/deniss_mirosnikovs/lesson_3/level_4/task_13/Book.java
@@ -0,0 +1,25 @@
+package students.deniss_mirosnikovs.lesson_3.level_4.task_13;
+
+class Book {
+
+    String title;
+
+    Book(String bookTitle) {
+        this.title = bookTitle;
+    }
+
+    String getTitle() {
+        return this.title;
+    }
+
+}
+
+class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+}
