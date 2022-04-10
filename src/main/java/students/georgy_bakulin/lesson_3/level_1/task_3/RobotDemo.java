234dfb3d66c34fb0c2ddfc1b8db3ed4b91ecdffd
georgy <georgy>
2022-04-11 01:01:55 +0300

    Lesson 3,Level 1,Task 3

---
+++ src/main/java/students/georgy_bakulin/lesson_3/level_1/task_3/RobotDemo.java
@@ -0,0 +1,14 @@
+package students.georgy_bakulin.lesson_3.level_1.task_3;
+
+
+
+public class RobotDemo {
+
+
+    public RobotDemo() {
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
