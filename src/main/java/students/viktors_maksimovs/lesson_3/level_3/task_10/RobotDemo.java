c8a7dfdf49b9ac7b85f9fdccaaa5d55cd78bc218
vmaksimovs <viktors.maksimovs@gmail.com>
2022-04-10 10:57:28 +0300

    Lesson 3, Level 2, Task 7 - Task 9

---
+++ src/main/java/students/viktors_maksimovs/lesson_3/level_3/task_10/RobotDemo.java
@@ -0,0 +1,14 @@
+package students.viktors_maksimovs.lesson_3.level_3.task_10;
+
+class RobotDemo {
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot("Rider");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+        Robot robot2 = new Robot("John");
+        robot2.sayHello();
+        robot2.sayYourName();
+    }
+}
