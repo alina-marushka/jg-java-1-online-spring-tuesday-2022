8bb5f654086cb014c3a5b6f67bfbbe796219a6dc
georgy <georgy>
2022-04-11 01:28:56 +0300

    Lesson 3,Level 1,Task 1-6

--- src/main/java/students/georgy_bakulin/lesson_3/level_1/task_1/Robot.java
+++ src/main/java/students/georgy_bakulin/lesson_3/level_1/task_1/Robot.java
@@ -2,8 +2,14 @@ package students.georgy_bakulin.lesson_3.level_1.task_1;
 
 public class Robot {
 
-
-    public void sayHello() {
+    public void sayHello(){
         System.out.println("Hello!");
+
+    }
+    public void sayYourName(){
+        System.out.println("My name is ROBO");
+    }
+
+    public Robot() {
     }
 }
