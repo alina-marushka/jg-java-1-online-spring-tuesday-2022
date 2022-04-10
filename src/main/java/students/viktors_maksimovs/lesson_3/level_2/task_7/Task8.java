baad8f24774fae7d9ae4d036fb27b2b9031f263d
vmaksimovs <viktors.maksimovs@gmail.com>
2022-04-10 10:37:10 +0300

    Lesson 3, Level 2, Task 7 - Task 9

---
+++ src/main/java/students/viktors_maksimovs/lesson_3/level_2/task_7/Task8.java
@@ -0,0 +1,10 @@
+package students.viktors_maksimovs.lesson_3.level_2.task_7;
+
+class Task8 {
+    public static void main(String[] args) {
+        String userName = "Viktor";
+        String greeting = "Hi " + userName + "!";
+
+        System.out.println(greeting);
+    }
+}
