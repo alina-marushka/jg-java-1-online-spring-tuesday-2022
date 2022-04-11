19e82d45172e79eb4a124c2c7417b8f840ffd595
Sergey N <snichiporenko@gmail.com>
2022-04-11 20:32:31 +0300

    homework

---
+++ src/main/java/students/sergejs_nichiporenko/lesson_3/level_2/Task.java
@@ -0,0 +1,19 @@
+package students.sergejs_nichiporenko.lesson_3.level_2;
+
+import java.util.Scanner;
+
+public class Task {
+
+    public static void main(String[] args) {
+        String myName = "Sergey";
+        System.out.println(myName);
+        System.out.println("Hi " + myName + "!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello, what is your name?");
+        String UserName = scanner.next();
+        System.out.println("Hello " + UserName + "!");
+    }
+
+}
