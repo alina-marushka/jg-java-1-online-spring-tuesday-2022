fccbeb992f46c1df49f85b7a47413fd3eceb448c
daniilsjb <daniilb@outlook.com>
2022-04-11 14:27:46 +0300

    Remove 'public' before class

--- src/main/java/students/georgy_bakulin/lesson_3/level_1/task_1/Robot.java
+++ src/main/java/students/georgy_bakulin/lesson_3/level_1/task_1/Robot.java
@@ -1,6 +1,6 @@
 package students.georgy_bakulin.lesson_3.level_1.task_1;
 
-public class Robot {
+class Robot {
 
     public void sayHello(){
         System.out.println("Hello!");
--- src/main/java/students/georgy_bakulin/lesson_3/level_1/task_1/RobotDemo.java
+++ src/main/java/students/georgy_bakulin/lesson_3/level_1/task_1/RobotDemo.java
@@ -1,6 +1,6 @@
 package students.georgy_bakulin.lesson_3.level_1.task_1;
 
-public class RobotDemo {
+class RobotDemo {
     public static void main(String[] args) {
 
         Robot robot1 = new Robot();
