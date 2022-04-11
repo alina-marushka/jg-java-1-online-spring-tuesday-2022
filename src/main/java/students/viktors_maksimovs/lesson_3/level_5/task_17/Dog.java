6071ed87049e4d605c2964105997890a90cd8060
vmaksimovs <viktors.maksimovs@gmail.com>
2022-04-11 20:14:33 +0300

    Lesson 3, Level 5, Task 17, Task 18 - Task 21

---
+++ src/main/java/students/viktors_maksimovs/lesson_3/level_5/task_17/Dog.java
@@ -0,0 +1,18 @@
+package students.viktors_maksimovs.lesson_3.level_5.task_17;
+
+class Dog {
+
+    String name;
+
+
+    Dog(String name) {
+        this.name = name;
+
+    }
+    void voice() {
+        System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
+
+    }
+}
