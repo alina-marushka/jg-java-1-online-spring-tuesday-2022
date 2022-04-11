99ed48b2ee0c93e57e4c6611e14a4cb3b855d4cc
georgy <georgy>
2022-04-11 11:42:47 +0300

    Lesson 3,Level 5,Task 18

---
+++ src/main/java/students/georgy_bakulin/lesson_3/level_5/task_18/Dog.java
@@ -0,0 +1,18 @@
+package students.georgy_bakulin.lesson_3.level_5.task_18;
+
+public class Dog {
+
+
+   int age;
+    String name;
+
+    public Dog(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    void voice(){
+        System.out.println("Dog's name is " + name + " " + "and"  + " " + "Dog's age is  " + age);
+
+    }
+}
+
