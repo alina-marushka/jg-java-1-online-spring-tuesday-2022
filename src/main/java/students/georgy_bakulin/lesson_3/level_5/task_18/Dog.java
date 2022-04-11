1e086a7c092bebd69bc13e5d0fd3212c934092a6
georgy <georgy>
2022-04-11 20:49:18 +0300

    Lesson 3,Level 5,Task 18

--- src/main/java/students/georgy_bakulin/lesson_3/level_5/task_18/Dog.java
+++ src/main/java/students/georgy_bakulin/lesson_3/level_5/task_18/Dog.java
@@ -1,6 +1,6 @@
 package students.georgy_bakulin.lesson_3.level_5.task_18;
 
-public class Dog {
+ class Dog {
 
 
    int age;
--- src/main/java/students/georgy_bakulin/lesson_3/level_5/task_18/DogDemo.java
+++ src/main/java/students/georgy_bakulin/lesson_3/level_5/task_18/DogDemo.java
@@ -1,6 +1,6 @@
 package students.georgy_bakulin.lesson_3.level_5.task_18;
 
-public class DogDemo {
+class DogDemo {
 
     public static void main(String[] args) {
 
