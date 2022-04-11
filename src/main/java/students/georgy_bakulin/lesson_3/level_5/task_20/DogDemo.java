6f7c96f38d70991f1c20bce077cc653b8558ff57
georgy <georgy>
2022-04-11 12:47:00 +0300

    Lesson 3,Level 5,Task 20

---
+++ src/main/java/students/georgy_bakulin/lesson_3/level_5/task_20/DogDemo.java
@@ -0,0 +1,9 @@
+package students.georgy_bakulin.lesson_3.level_5.task_20;
+
+public class DogDemo {
+    public static void main(String[] args) {
+
+        Dog dog= new Dog("Pluto",3,"brown");
+        dog.voice();
+    }
+}
