3fbb8fc1c7b12f77b17b7ffc69b7ba092bc58d84
Prolog952 <prolog952@gmail.com>
2022-04-15 17:03:53 +0300

    vlad-stus-lesson-3-level-5-task-26-Dog-ChangeColor

---
+++ src/main/java/students/vlad_stus/lesson_3/level_7/Task_26/Truck.java
@@ -0,0 +1,18 @@
+package students.vlad_stus.lesson_3.level_7.Task_26;
+
+class Truck {
+    String truckPlate;
+    String trailerPlate;
+    int axles;
+    int weight;
+
+    Truck(){
+
+    }
+
+    Truck(String truckPlate, int axles, int weight ){
+        this.truckPlate =truckPlate;
+        this.axles = axles;
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/students/vlad_stus/lesson_3/level_7/Task_26/TruckDemo.java
@@ -0,0 +1,4 @@
+package students.vlad_stus.lesson_3.level_7.Task_26;
+
+ class TruckDemo {
+}
