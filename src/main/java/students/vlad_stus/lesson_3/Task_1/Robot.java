57502a8d33083a5f060077e7bd5ecf6d3d2e92ee
Prolog952 <prolog952@gmail.com>
2022-04-15 12:52:19 +0300

    vlad-stus-lesson-3-level-1-task_1-Robot

---
+++ src/main/java/students/vlad_stus/lesson_3/Task_1/Robot.java
@@ -0,0 +1,9 @@
+package students.vlad_stus.lesson_3.Task_1;
+
+public class Robot {
+
+
+    void sayHello(){
+        System.out.println("Hello my Master");
+    }
+}
---
+++ src/main/java/students/vlad_stus/lesson_3/Task_1/RobotDemo.java
@@ -0,0 +1,10 @@
+package students.vlad_stus.lesson_3.Task_1;
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robotOne = new Robot ();
+        robotOne.sayHello();
+    }
+}
