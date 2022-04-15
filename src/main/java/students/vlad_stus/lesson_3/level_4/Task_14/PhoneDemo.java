83144e27e5ae60cede391640fb4d72f8093070bb
Prolog952 <prolog952@gmail.com>
2022-04-15 14:04:21 +0300

    vlad-stus-lesson-3-level-4-task-14-Phone

---
+++ src/main/java/students/vlad_stus/lesson_3/level_4/Task_14/PhoneDemo.java
@@ -0,0 +1,11 @@
+package students.vlad_stus.lesson_3.level_4.Task_14;
+
+class PhoneDemo {
+
+    public static void main(String[] args) {
+        Phone myPhone = new Phone("Huawei");
+        String phoneModel = myPhone.getModel();
+        System.out.println("Phone model = " + phoneModel);
+    }
+
+}
