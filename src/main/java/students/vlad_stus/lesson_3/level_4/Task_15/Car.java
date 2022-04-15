9e4c98425f65e01c1daf53da73781288485f5890
Prolog952 <prolog952@gmail.com>
2022-04-15 14:05:54 +0300

    vlad-stus-lesson-3-level-4-task-15-Car

---
+++ src/main/java/students/vlad_stus/lesson_3/level_4/Task_15/Car.java
@@ -0,0 +1,15 @@
+package students.vlad_stus.lesson_3.level_4.Task_15;
+
+class Car {
+
+    String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+    }
+
+}
