9901edf94f0d26e3e5c92411c2cff9393fa7083a
Prolog952 <prolog952@gmail.com>
2022-04-15 14:12:29 +0300

    vlad-stus-lesson-3-level-4-task-16-BankAccount

---
+++ src/main/java/students/vlad_stus/lesson_3/level_4/Task_16/BankAccount.java
@@ -0,0 +1,27 @@
+package students.vlad_stus.lesson_3.level_4.Task_16;
+
+class BankAccount {
+
+    String ownerFirstName;
+    String ownerLastName;
+    long moneyAmount;
+
+    BankAccount(String ownerFirstName, String ownerLastName, long moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerFirstName;
+    }
+
+    long getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+}
