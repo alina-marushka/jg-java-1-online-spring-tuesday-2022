5aafefb570f23feca8061971af3d1fd350f3ca26
Prolog952 <prolog952@gmail.com>
2022-04-17 09:57:59 +0300

    vlad-stus-lesson-3-level-7-task-29-BankAccaount

---
+++ src/main/java/students/vlad_stus/lesson_3/Task_29/BankAccount.java
@@ -0,0 +1,21 @@
+package students.vlad_stus.lesson_3.Task_29;
+
+class BankAccount {
+
+ String owner;
+ int money;
+
+ BankAccount(String owner,  int money) {
+  this.owner = owner;
+  this.money=money;
+ }
+
+ String getOwner() {
+  return this.owner;
+ }
+
+int getMoney() {
+  return this.money;
+ }
+
+}
