6816c96ba4cd5abaf74e33193db3008a24e2e0f1
Prolog952 <prolog952@gmail.com>
2022-04-17 09:22:15 +0300

    vlad-stus-lesson-3-level-5-task-27-Truck_Demo

--- src/main/java/students/vlad_stus/lesson_3/level_7/Task_26/Truck.java
+++ src/main/java/students/vlad_stus/lesson_3/level_7/Task_26/Truck.java
@@ -1,18 +1,24 @@
 package students.vlad_stus.lesson_3.level_7.Task_26;
 
 class Truck {
+    String truckName;
     String truckPlate;
-    String trailerPlate;
     int axles;
     int weight;
 
-    Truck(){
 
-    }
 
-    Truck(String truckPlate, int axles, int weight ){
+    Truck( String truckName, String truckPlate, int axles, int weight ){
+        this.truckName = truckName;
         this.truckPlate =truckPlate;
         this.axles = axles;
         this.weight = weight;
+
+    }
+
+    void printinformation(){
+        System.out.println("Truck ready for working: " + truckPlate);
+        System.out.println("Truck plate : " + truckPlate );
+
     }
 }
