06909cbc0890ea25e8fee7bffb6383dfec5ba3dc
roman <romanbogomol@gmail.com>
2022-04-21 00:56:21 +0300

    Lesson 5, Level 2, Task 10.

---
+++ src/main/java/students/romans_bogomols/lesson_5/level_2/task_10/Numbers.java
@@ -0,0 +1,18 @@
+package students.romans_bogomols.lesson_5.level_2.task_10;
+
+public class Numbers {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+        numbers[0] = 13;
+        numbers[1] = 3;
+        numbers[2] = 99481;
+
+        for (int number : numbers) {
+            System.out.print(number);
+            System.out.print(" ");
+        }
+    }
+}
