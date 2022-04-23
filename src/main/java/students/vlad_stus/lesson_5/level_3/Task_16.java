249d53617a959bca3e2536522b3c7ec6ad0e5101
Prolog952 <prolog952@gmail.com>
2022-04-23 15:46:45 +0300

    vlad-stus-lesson-5-level-3-task-16

---
+++ src/main/java/students/vlad_stus/lesson_5/level_3/Task_16.java
@@ -0,0 +1,10 @@
+package students.vlad_stus.lesson_5.level_3;
+
+ class Task_16 {
+  public static void main(String[] args) {
+
+   for (int i = 0; i < 10; i++) {
+    System.out.println(i);
+   }
+  }
+}
