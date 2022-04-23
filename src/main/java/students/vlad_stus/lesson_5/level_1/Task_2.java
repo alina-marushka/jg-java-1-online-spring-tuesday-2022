a0af6fdd246817af9222286ce685304976c57f55
Prolog952 <prolog952@gmail.com>
2022-04-23 13:05:28 +0300

    vlad-stus-lesson-5-level-1-task-2

---
+++ src/main/java/students/vlad_stus/lesson_5/level_1/Task_2.java
@@ -0,0 +1,11 @@
+package students.vlad_stus.lesson_5.level_1;
+
+import java.util.Arrays;
+
+class Task_2 {
+
+     public static void main(String[] args) {
+         byte[] numbers = new byte[10];
+         System.out.println(Arrays.toString(numbers));
+}
+}
