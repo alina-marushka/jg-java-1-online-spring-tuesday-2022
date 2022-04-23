911b1c0fa3e763f3ae1183a82957521c7d25a429
Prolog952 <prolog952@gmail.com>
2022-04-23 13:18:45 +0300

    vlad-stus-lesson-5-level-1-task-8

---
+++ src/main/java/students/vlad_stus/lesson_5/level_1/Task_8.java
@@ -0,0 +1,13 @@
+package students.vlad_stus.lesson_5.level_1;
+
+ class Task_8 {
+
+
+     public static void main(String[] args) {
+         int[] numbers = new int[3];
+         System.out.println(numbers[0] =2);
+         System.out.println(numbers[1] = 9);
+         System.out.println(numbers[2] =1);
+     }
+
+ }
