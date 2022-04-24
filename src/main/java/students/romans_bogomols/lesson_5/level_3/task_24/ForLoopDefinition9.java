8994b24fff3862eb167375eef8d6b5f13115a57b
roman <romanbogomol@gmail.com>
2022-04-24 14:05:07 +0300

    Lesson 5, Level 3, Task 16-24

---
+++ src/main/java/students/romans_bogomols/lesson_5/level_3/task_24/ForLoopDefinition9.java
@@ -0,0 +1,10 @@
+package students.romans_bogomols.lesson_5.level_3.task_24;
+
+class ForLoopDefinition9 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 101 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
