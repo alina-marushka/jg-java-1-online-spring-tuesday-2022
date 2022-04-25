83fa236683ab138428d706e09a60e1246a3afc7c
Sergey N <snichiporenko@gmail.com>
2022-04-25 21:15:31 +0300

    homework

---
+++ src/main/java/students/vlad_stus/level_3/Task_8.java
@@ -0,0 +1,27 @@
+package students.vlad_stus.level_3;
+
+import java.util.Scanner;
+
+class Task_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое целое число ");
+        int numberA = scanner.nextInt();
+
+        System.out.println("Введите второе целое число ");
+        int numberB = scanner.nextInt();
+
+        System.out.println("Введите третье целое число ");
+        int numberC = scanner.nextInt();
+
+        if (numberA < numberB && numberB < numberC) {
+            System.out.println("increasing");
+
+        } else if (numberA > numberB && numberB > numberC) {
+            System.out.println("decreasing");
+
+        } else {
+            System.out.println("neither");
+        }
+    }
+}
