e92a68422dddff1dfbc10aedb6f01a1b43ac4c2a
Prolog952 <prolog952@gmail.com>
2022-04-25 22:35:24 +0300

    vlad-stus-lesson-5-level-4-task-31-Arrays

---
+++ src/main/java/students/vlad_stus/lesson_5/level_5/Task_31/Arrays.java
@@ -0,0 +1,18 @@
+package students.vlad_stus.lesson_5.level_5.Task_31;
+
+import java.util.Scanner;
+
+ class Arrays {
+
+  Scanner scanner;
+
+  Arrays(){
+   this. scanner = new Scanner(System.in);
+  }
+
+  int [] create (int length){
+   int[] massive = new int[length];
+   return massive;
+  }
+  }
+
---
+++ src/main/java/students/vlad_stus/lesson_5/level_5/Task_31/ArraysTest.java
@@ -0,0 +1,4 @@
+package students.vlad_stus.lesson_5.level_5.Task_31;
+
+ class ArraysTest {
+}
