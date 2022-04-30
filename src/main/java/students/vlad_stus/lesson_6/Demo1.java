1740f3bf4abc77f32c0a1e750180572a2ead2ea1
Prolog952 <prolog952@gmail.com>
2022-04-30 12:28:10 +0300

    vlad-stus-lesson-6-level-6-Demo1

---
+++ src/main/java/students/vlad_stus/lesson_6/Demo1.java
@@ -0,0 +1,12 @@
+package students.vlad_stus.lesson_6;
+
+class Demo1 {
+    public static void main(String[] args) {
+
+        int a = 7;
+        while (a <98){
+            System.out.println(a + " ");
+            a += 7;
+        }
+    }
+}
