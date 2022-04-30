525c5571d88eef9eb56a116dfbffa336a9ecd690
Prolog952 <prolog952@gmail.com>
2022-04-30 12:52:33 +0300

    vlad-stus-lesson-6-level-6-lesson-1-task-6

---
+++ src/main/java/students/vlad_stus/lesson_6/level_1/task_6/NumberUtils.java
@@ -0,0 +1,13 @@
+package students.vlad_stus.lesson_6.level_1.task_6;
+
+class NumberUtils {
+
+    boolean isEven(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ src/main/java/students/vlad_stus/lesson_6/level_1/task_6/NumberUtilsTest.java
@@ -0,0 +1,14 @@
+package students.vlad_stus.lesson_6.level_1.task_6;
+
+ class NumberUtilsTest {
+
+     NumberUtilsTest utilTest = new NumberUtilsTest();
+     utilTest.isEvenTest();
+
+
+     void isEvenTest() {
+         NumberUtils testUtil = new NumberUtils();
+         int expectResult = 10;
+         int acutalResult = testUtil.isEven(expectResult);
+     }
+ }
