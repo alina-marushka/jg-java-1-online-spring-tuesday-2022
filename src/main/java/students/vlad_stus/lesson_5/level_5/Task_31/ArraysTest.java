802d34bc8225eaff1dfbb983d917d23fe9ccd67a
Prolog952 <prolog952@gmail.com>
2022-04-30 11:53:53 +0300

    vlad-stus-lesson-5-level-5-task-36-findMax

--- src/main/java/students/vlad_stus/lesson_5/level_5/Task_31/ArraysTest.java
+++ src/main/java/students/vlad_stus/lesson_5/level_5/Task_31/ArraysTest.java
@@ -13,9 +13,9 @@ package students.vlad_stus.lesson_5.level_5.Task_31;
 
 
 
-
+  Arrays arrays = new Arrays();
   void shouldCreateArray() {
-   Arrays arrays = new Arrays();
+
    int expectedResult = 10;
    int[] demoArrays = arrays.create(expectedResult);
    int actualResult =demoArrays.length;
@@ -23,21 +23,21 @@ package students.vlad_stus.lesson_5.level_5.Task_31;
    }
 
 
-   void checkResult(int expectedResult, int actualResult, String testName){
-   if( expectedResult == actualResult){
-    System.out.println(testName + "DONE ");}
-   else{
-    System.out.println(testName + "FAILED");
+   void checkResult(int expectedResult, int actualResult, String testName) {
+    if (expectedResult == actualResult) {
+     System.out.println(testName + "DONE ");
+    } else {
+     System.out.println(testName + "FAILED");
+    }
    }
    void shouldFindMaxNumber(int) {
-     Arrays arrays = new Arrays();
      int[] testArray = {33, 20, 106, 99, 564};
      int expectedResult = testArray[4];
-     int actualResult = util.maxNumbers(testArray);
+     int actualResult =  arrays.findMax(testArray);
      checkResult(expectedResult, actualResult, "Max Number test");
 
     }
    }
-}
+
 
 
