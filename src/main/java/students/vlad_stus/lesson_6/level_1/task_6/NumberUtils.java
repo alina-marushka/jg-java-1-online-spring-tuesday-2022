e328e58f118eabcabf56787fd1f7c963d23af5b8
Prolog952 <prolog952@gmail.com>
2022-04-30 13:07:36 +0300

    vlad-stus-lesson-6-level-6-lesson-1-task-6

--- src/main/java/students/vlad_stus/lesson_6/level_1/task_6/NumberUtils.java
+++ src/main/java/students/vlad_stus/lesson_6/level_1/task_6/NumberUtils.java
@@ -3,11 +3,7 @@ package students.vlad_stus.lesson_6.level_1.task_6;
 class NumberUtils {
 
     boolean isEven(int number) {
-        if (number % 2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return (number % 2 == 0);
     }
-
 }
+
--- src/main/java/students/vlad_stus/lesson_6/level_1/task_6/NumberUtilsTest.java
+++ src/main/java/students/vlad_stus/lesson_6/level_1/task_6/NumberUtilsTest.java
@@ -2,16 +2,23 @@ package students.vlad_stus.lesson_6.level_1.task_6;
 
  class NumberUtilsTest {
 
-     NumberUtilsTest utilTest = new NumberUtilsTest();
-     utilTest.isEvenTest();
+     public static void main(String[] args) {
+         NumberUtilsTest  numberUtilsTest = new NumberUtilsTest();
+         numberUtilsTest.isEvenTest();
+
+     }
 
 
      void isEvenTest() {
          NumberUtils testUtil = new NumberUtils();
-         int expectResult = 10;
-         int acutalResult = testUtil.isEven(expectResult);
-         if(expectResult == acutalResult){
-             System.out.println("Test Done!");
+         boolean actualResult = testUtil.isEven(2);
+         checkTestResult("isEvenTest", actualResult);
+         }
+ void checkTestResult(String testName, boolean result) {
+         if (result) {
+             System.out.println("\"" + testName + "\"" + " test = OK");
+         } else {
+             System.out.println("\"" + testName + "\"" + " test = FAIL");
          }
      }
- }
+     }
