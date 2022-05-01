f6322b5d03dcb3caf2799bf958d5e60a58a5466c
vmaksimovs <viktors.maksimovs@gmail.com>
2022-05-01 16:08:37 +0300

    Lesson 6 fixes

--- src/main/java/students/viktors_maksimovs/lesson_6/level_1/Task2.java
+++ src/main/java/students/viktors_maksimovs/lesson_6/level_1/Task2.java
@@ -4,9 +4,10 @@ class WhileDefinition2 {
 
     public static void main(String[] args) {
         int i = 1;
-        while (i <= 100)
+        while (i <= 100) {
             System.out.println(i);
-        i++;
+            i++;
+        }
     }
 }
 
