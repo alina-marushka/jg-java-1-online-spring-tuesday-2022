2cf06bda1865280b44b83610f835a5b9dbcbdfb7
vmaksimovs <viktors.maksimovs@gmail.com>
2022-05-07 14:43:51 +0300

    Lesson 6, level 5
    застрял на Draw

---
+++ src/main/java/students/viktors_maksimovs/lesson_6/level_5/TicTacToe.java
@@ -0,0 +1,52 @@
+package students.viktors_maksimovs.lesson_6.level_5;
+
+class TicTacToe {
+
+    public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
+
+        if (field[0][0] == playerToCheck && field[0][1] == playerToCheck && field[0][2] == playerToCheck) {
+            return true;
+        } else if (field[1][0] == playerToCheck && field[1][1] == playerToCheck && field[1][2] == playerToCheck) {
+            return true;
+        } else
+            return field[2][0] == playerToCheck && field[2][1] == playerToCheck && field[2][2] == playerToCheck;
+    }
+
+    public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
+        if (field[0][0] == playerToCheck && field[1][0] == playerToCheck && field[2][0] == playerToCheck) {
+            return true;
+        } else if (field[0][1] == playerToCheck && field[1][1] == playerToCheck && field[2][1] == playerToCheck) {
+            return true;
+        } else
+            return field[0][2] == playerToCheck && field[1][2] == playerToCheck && field[2][2] == playerToCheck;
+    }
+
+    public boolean isWinPositionForDiagonals(int[][] field, int playerToCheck) {
+        if (field[0][0] == playerToCheck && field[1][1] == playerToCheck && field[2][2] == playerToCheck) {
+            return true;
+        } else
+            return field[2][0] == playerToCheck && field[1][1] == playerToCheck && field[0][2] == playerToCheck;
+    }
+
+    public boolean isWinPosition(int[][] board, int playerToCheck) {
+
+        return isWinPositionForHorizontals(board, playerToCheck) || isWinPositionForVerticals(board, playerToCheck)
+                || isWinPositionForDiagonals(board, playerToCheck);
+    }
+
+    public boolean isDrawPosition(int[][] field){
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (field[i][j] == -1)
+                    return !(isWinPosition(field, 0) || isWinPosition(field, 1));
+            }
+        }
+        return  false;
+    }
+
+}
+
+
+
